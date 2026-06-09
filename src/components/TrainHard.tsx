77b4bd2946a8eb4d6edf332311ba8a511c4cb72b
Nathan Ahrens-Townsend <nathan.ahrens.townsend@gmail.com>
2026-06-09 13:43:07 -0500

    Optimized image

--- src/components/TrainHard.tsx
+++ src/components/TrainHard.tsx
@@ -4,7 +4,7 @@ export default function TrainHard() {
   return (
     <div className="train-hard">
       <img
-        src="/src/assets/images/train-hard.png"
+        src="/src/assets/images/train-hard.webp"
         className="train-hard-image"
         alt="Description of image"
       />
